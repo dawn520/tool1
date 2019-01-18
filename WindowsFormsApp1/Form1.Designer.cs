fba2e60af4026cd43901e8635ecfe37149c381c3
xixi zhou <admin@zhouchenxi.cn>
2019-01-18 18:51:37 +0800

    .

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(351, 28);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(413, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "选择Excel文件：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "选择图片目录：";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(42, 196);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(351, 28);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(42, 304);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(458, 26);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "生成进度：";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(42, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(458, 57);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "开始";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 481);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "17duu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using NPOI;
+using NPOI.SS.UserModel;
+using NPOI.HSSF.UserModel;
+using NPOI.XSSF.UserModel;
+using System.IO;
+using NPOI.XWPF.UserModel;
+using NPOI.Util;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public int process { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Changed(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();     //显示选择文件对话框
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "excel文件 (*.xls;*.xlsx)|*.xls;*.xlsx";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = openFileDialog1.FileName;          //显示文件路径
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "请选择图片所在文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (dialog.SelectedPath.Trim() != "")
+                    this.textBox2.Text = dialog.SelectedPath.Trim();
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string fileName = this.textBox1.Text;
+            string imagePath = this.textBox2.Text;
+
+            if (fileName == "")
+            {
+                MessageBox.Show("请选择一个Excel文件！");
+                return;
+            }
+            if (imagePath == "")
+            {
+                MessageBox.Show("请选择图片所在目录！");
+                return;
+            }
+
+            //方法一：使用Thread类
+            ThreadStart threadStart = new ThreadStart(doIt);//通过ThreadStart委托告诉子线程执行什么方法　
+            Thread thread = new Thread(threadStart);
+            thread.Start();//启动新线程
+
+
+        }
+
+        private void doIt()
+        {
+            string fileName = this.textBox1.Text;
+            string imagePath = this.textBox2.Text;
+
+            IWorkbook workbook = null;  //新建IWorkbook对象  
+
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            if (fileName.IndexOf(".xlsx") > 0) // 2007版本  
+            {
+                workbook = new XSSFWorkbook(fileStream);  //xlsx数据读入workbook  
+            }
+            else if (fileName.IndexOf(".xls") > 0) // 2003版本  
+            {
+                workbook = new HSSFWorkbook(fileStream);  //xls数据读入workbook  
+            }
+            //word文档
+            XWPFDocument doc = new XWPFDocument(); //文档
+
+            ISheet sheet = workbook.GetSheetAt(0);  //获取第一个工作表  
+            IRow row;// = sheet.GetRow(0);            //新建当前工作表行数据  
+            for (int i = 1; i < sheet.LastRowNum; i++)  //对工作表每一行  
+            {
+                row = sheet.GetRow(i);   //row读入第i行数据  
+                if (row != null)
+                {
+                    //标题
+                    string title = row.GetCell(0).ToString();
+                    Console.WriteLine(title);
+
+                    XWPFParagraph p1 = doc.CreateParagraph();
+                    p1.Alignment = ParagraphAlignment.LEFT;
+
+                    XWPFRun r1 = p1.CreateRun();
+                    r1.SetText(title);
+
+                    //图片
+                    string pictureaName = row.GetCell(1).ToString();
+                    Console.WriteLine(pictureaName);
+
+                    XWPFParagraph p2 = doc.CreateParagraph();
+                    p2.Alignment = ParagraphAlignment.LEFT;
+
+                    FileStream image = new FileStream(imagePath + "/" + pictureaName, FileMode.Open, FileAccess.Read);
+
+                    XWPFRun r2 = p2.CreateRun();
+                    r2.AddPicture(image, 5, imagePath + "/" + pictureaName, Units.ToEMU(500), Units.ToEMU(500));
+                    r2.AddBreak();
+                }
+                double d = (double) ((double)i / (double)sheet.LastRowNum) * 100;
+                process = (int)Math.Ceiling(Convert.ToDouble(d));
+  
+                Console.WriteLine(process);
+
+                this.progressBar1.BeginInvoke(new EventHandler((sender, e) =>
+                {
+                    Console.WriteLine(process * 100);
+                    this.progressBar1.Value = process;
+                }), null);
+            }
+            Console.ReadLine();
+            fileStream.Close();
+            workbook.Close();
+
+            MessageBox.Show("ha");
+
+            FileStream out1 = new FileStream(@"c:\simple.docx", FileMode.Create);
+            doc.Write(out1);
+            out1.Close();
+        }
+    }
+}
